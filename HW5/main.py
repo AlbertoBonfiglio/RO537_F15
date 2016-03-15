408a51e2e38dbfacfc468c667923d4b2caec61fc
alberto.bonfiglio@gmail.com <R0b0tics!!152>
2016-03-15 08:50:50 -0700

    More work done on integration of GA, NN and pendulum

--- HW5/main.py
+++ HW5/main.py
@@ -14,9 +14,9 @@ from HW5.classes.GeneticAlgorithm import Population
 
 
 
-def pendulumTest(timeslice=0.001, tmax=100):
+def pendulumForceTest(timeslice=0.001, tmax=100):
     pendulum = InvertedPendulum()
-    for n in np.arange(-200, 200, 100):
+    for n in np.arange(-5, 5, 1):
         states, time = pendulum.time_to_ground(u=n, tmax=tmax, timeslice=timeslice)
         n=0
         for state in states:
@@ -30,6 +30,18 @@ def pendulumTest(timeslice=0.001, tmax=100):
         x, y = transform(theta)
 
        # showGraph(x, y, cart, 0.001, "Relative motion of cart and pendulum u={0}".format(n))
+
+
+def pendulumTest(timeslice=0.001, tmax=100):
+    pendulum = InvertedPendulum()
+    for n in np.arange(-5, 5, 1):
+        states, time = pendulum.time_to_ground(u=n, tmax=tmax, timeslice=timeslice)
+
+        theta = (state.theta for state in states)
+        cart =  (state.x for state in states)
+        x, y = transform(theta)
+
+        showGraph(x, y, cart, 0.001, "Cart-Pendulum System --> U={0}".format(n))
 
 
 def transform(theta):
@@ -50,7 +62,8 @@ def showGraph(x, y, cart, timeslice=0.01, caption=""):
     #TODO make sure the x axis reflects the time it takes to drop
 
     fig, axes = plt.subplots(nrows=1, ncols=3, sharex=False, sharey=False,
-                             tight_layout=True, figsize=(9, 4.5))
+                              figsize=(9, 4.5))
+    #tight_layout=True,
     fig.suptitle(caption, fontsize=18, fontweight='bold')
 
     ax = axes[0]
@@ -122,7 +135,9 @@ def run_controller(trials=10, epochs =250, timeslice=0.002, tmax=0.2, threshold 
         while force == 0:
             force = np.random.randint(-5, 5)
 
-        initial_state, time = pendulum.get_State(u=force, tmax=tmax, timeslice=timeslice)
+        initial_state = State()
+        initial_state.theta = pi
+        initial_state, time = pendulum.get_State(u=force, initialstate=initial_state, tmax=tmax, timeslice=timeslice)
         print('Force={1:3f} -Theta={0:4f}'.format(initial_state[-1].theta, force))
 
         reward_array = []
@@ -154,6 +169,71 @@ def run_controller(trials=10, epochs =250, timeslice=0.002, tmax=0.2, threshold 
         main_array.append((reward_array, population_array))
 
     return main_array
+
+
+def run_controller2(trials=10, epochs =250, timeslice=0.002, tmax=0.2, threshold =((-pi/2), (pi/2))):
+    #step 0: set up Neural network
+    #step 1: Start pendulum with random force
+    #step 2: at n milliseconds take state
+    #step 3: pass state to genetic algorithm
+    #step 4: GA runs n epochs and evaluates what gives the best output
+    #step 5: GA passes weights to NN to evaluate a force to apply to pendulum
+    #Step 6: NN applies force to pendulum
+    #Step 7: Goto step 2
+
+    #TODO run multiple simulations, collect avg data and error and produce graphs
+    MAX_REWARD = 1000
+    pendulum = InvertedPendulum()
+    NN = NEvoNetwork(inputs=6, outputs=1, hiddenlayers=1,  hiddenneurons=12, inputweights=6, activation=TanhActivation)
+    ga = Population(NN=NN, size=30)
+
+    main_array = []
+    reward_array =[]
+    smallest_angle = pi
+
+    for trial in range(0, trials):
+        ga.create(size=30)
+        force = np.random.randint(-5, 5)
+        while force == 0:
+            force = np.random.randint(-5, 5)
+
+        initial_state = State()
+        initial_state.theta = pi
+        initial_state, time = pendulum.get_State(u=force, initialstate=initial_state, tmax=tmax, timeslice=timeslice)
+        print('Force={1:3f} -Theta={0:4f}'.format(initial_state[-1].theta, force))
+
+        reward_array = []
+        population_array = []
+        for epoch in range(0, epochs):
+            for induhvidual in ga.individuals:
+                NN.set_weights(induhvidual.alleles)
+                state = []
+                state.append(initial_state[-1])
+
+
+
+                airborne = True
+                while airborne:
+                    force = NN.get_outputs([state[-1].x, state[-1].xdot, state[-1].x2dot, state[-1].theta, state[-1].thetadot, state[-1].theta2dot])[0] * 5
+                    state, time = pendulum.get_State(u=force, initialstate=state[-1], tmax=tmax, timeslice=timeslice)
+
+                    if state[-1].theta < threshold[0] or state[-1].theta > threshold[1]:
+                        airborne = False
+                    else:
+                        induhvidual.set_fitness(1)
+
+                    if induhvidual.get_fitness() >= MAX_REWARD: break
+
+            reward_array.append(ga.getFittestIndividual().get_fitness())
+            population_array.append(ga.getPopulationFitness())
+            print('Trial: {0} - Epoch {1} --> Best fitness score = {2}, - Pop Fitness = {3}'
+                  .format(trial, epoch, ga.getFittestIndividual().get_fitness(), ga.getPopulationFitness()))
+            ga.evolve(epochs=1)
+
+        main_array.append((reward_array, population_array))
+
+    return main_array
+
 
 
 def plot_controller_run(data, trials, epochs, threshold):
@@ -198,12 +278,8 @@ def plot_controller_run(data, trials, epochs, threshold):
 if __name__ == '__main__':
 
     #pendulumTest(0.01)
-    #pendulumTest(0.001)
-    #pendulumTest(0.0001)
 
-    #pendulumTest(0.001, 0.001*200)
 
-    #nnmain()
     trials= 15
     epochs= 150
     threshold =((-pi/4), (pi/4))
